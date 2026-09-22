1894117847a18f6a56e447c74b72d150646a9dc1
Levys.LK <levyscastro@gmail.com>
2026-09-22 01:17:16 -0300

    refactor: encapsula inicialização da aplicação na função main

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ from app.interface.menu import Menu
 from app.utils.json_handler import check_temp_files
 from app.interface.inputs import ask_press_enter_to_continue
 
-order_repositiry = OrderRepository()
-products_repository = ProductsRepository()
-service = OrderService(order_repositiry, products_repository)
-menu = Menu(service)
+def main():
+    order_repository = OrderRepository()
+    products_repository = ProductsRepository()
+    service = OrderService(order_repository, products_repository)
+    menu = Menu(service)
 
-temp_files = check_temp_files()
-if temp_files:
-    print('Há arquivos temporários, não tratados, no sistema. Verifique antes de continuar!')
-    for item in temp_files:
-        print(item)
-    print()
-    ask_press_enter_to_continue()
+    temp_files = check_temp_files()
+    if temp_files:
+        print('Há arquivos temporários, não tratados, no sistema. Verifique antes de continuar!')
+        for item in temp_files:
+            print(item)
+        print()
+        ask_press_enter_to_continue()
 
-menu.execute()
+    menu.execute()
+
+if __name__ == "__main__":
+    main()
