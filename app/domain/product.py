cad8d901f89c5bbc27e05751dc42da782f9b86e0
Levys.LK <levyscastro@gmail.com>
2026-09-22 00:22:20 -0300

    fix: corrige nomenclaturas, mensagens e pequenos erros de texto

--- app/domain/product.py
+++ app/domain/product.py
@@ -39,7 +39,7 @@ class Product:
             char in ('.', ',', '/', '\\', '|', '*', '+')
             for char in value
         ):
-            raise ValueError('CLIENTE: Nome inválido.')
+            raise ValueError('PRODUTO: Nome inválido.')
 
         self._name = value.strip().lower()
 
